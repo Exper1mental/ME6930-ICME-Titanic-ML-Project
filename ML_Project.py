f095fa82ebc4215b8fa6b715d5a0409e13db1fda
vchaluv <81371404+vchaluv@users.noreply.github.com>
2021-04-05 09:54:59 -0400

    Update ML_Project.py

--- ML_Project.py
+++ ML_Project.py
@@ -18,6 +18,7 @@ import pandas as pd # Data processing
 import os # File locating
 import seaborn as sns # Constructing graphs
 import matplotlib.pyplot as plt # Plotting results
+import random as rnd # Random number generator
 
 
 ### Importing Data
@@ -123,9 +124,32 @@ for dataset in combine: # Perform this action for both the testing and training 
 # Removed any rows missing data. Should only remove 2 rows missing embarked data.
 combined_df = combined_df.dropna()
 
+## Making embarkation data usable
+for dataset in combine:
+    dataset['Embarked'] = dataset['Embarked'].map( {'S': 0, 'C': 1, 'Q': 2} ).astype(int)
+
 # Confirm there are no more entries missing data
 #print(combined_df.isnull().sum())
 
+### Feature Engineering
+
+## Title
+for dataset in combine: # Perform this action for both the testing and training datasets
+    dataset['Title'] = dataset['Title'].replace(['Lady', 'Countess','Capt', 'Col',\
+ 	'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare') # These are rare titles which may indicate a different relevance than other titles
+
+    dataset['Title'] = dataset['Title'].replace('Mlle', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Mme', 'Mrs')
+
+title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5} # Ensure it is numerical for ML purposes
+for dataset in combine:
+    dataset['Title'] = dataset['Title'].map(title_mapping)
+    dataset['Title'] = dataset['Title'].fillna(0)
+    
+## Family Size
+for dataset in combine: # Perform this action for both the testing and training datasets
+    dataset['FamilySize'] = dataset['SibSp'] + dataset['Parch'] + 1 # Creates a separate single variable for family size
 
 ## Split Embarked data into three boolean columns
 
@@ -148,7 +172,6 @@ combined_df = pd.get_dummies(combined_df)
 #plt.show()
 
 # Plot should show no missing entries, indicating the data is ready for use with ML
-
 
 ### Correlations
 
@@ -195,9 +218,8 @@ X_test = sc_X.transform(X_test)
 classifier=DecisionTreeClassifier(criterion="entropy",random_state=0)
 classifier.fit(X_train,y_train)
 y_pred=classifier.predict(X_test)
-acc=accuracy_score(y_test, y_pred)
-print(f'Decision Trees Accuracy: {round(acc*100,3)}%')
-
+acc_dt=accuracy_score(y_test, y_pred)
+print(f'Decision Trees Accuracy: {round(acc_dt*100,3)}%')
 
 # ML Algorithm 2: Random Forest
 from sklearn.ensemble import RandomForestClassifier
@@ -211,5 +233,51 @@ y = rf['Survived']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
 model = RandomForestClassifier(n_estimators=100, max_depth=10, random_state=0)
 model.fit(X_train, y_train)
-acc = model.score(X_test, y_test)
-print(f'Random Forest Accuracy: {round(acc*100,3)}%')
+acc_rf = model.score(X_test, y_test)
+print(f'Random Forest Accuracy: {round(acc_rf*100,3)}%')
+
+########################################################################
+##### Anish's ML Algorithms
+# Based on: https://www.kaggle.com/vinothan/titanic-model-with-90-accuracy; https://www.kaggle.com/startupsci/titanic-data-science-solutions#Titanic-Data-Science-Solutions
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
+
+# ML Algorithm 3: Linear Regression
+from sklearn.linear_model import LogisticRegression
+
+lr = combined_df.copy
+X_train_lr = lr.drop('Survived', axis=1)
+Y_train_lr = lr['Survived']
+
+logreg = LogisticRegression()
+logreg.fit(X_train_lr, Y_train_lr)
+Y_pred_lr = logreg.predict(X_test)
+acc_log = logreg.score(X_train_lr, Y_train_lr)
+print(f'Logistic Regression Accuracy: {round(acc_log*100,3)}%')
+
+coeff_df = pd.DataFrame(train_df.columns.delete(0)) # Details correlations between features for better understanding (I think we should try to do this for all of our models)
+coeff_df.columns = ['Feature']
+coeff_df["Correlation"] = pd.Series(logreg.coef_[0])
+coeff_df.sort_values(by='Correlation', ascending=False)
+
+# ML Algorithm 4: k-Nearest Neighbors (KNN)
+from sklearn.neighbors import KNeighborsClassifier
+
+knndf = combined_df.copy
+X_train_knn = knndf.drop('Survived', axis=1)
+Y_train_knn = knndf['Survived']
+
+knn = KNeighborsClassifier(n_neighbors = 3)
+knn.fit(X_train_knn, Y_train_knn)
+Y_pred_knn = knn.predict(X_test)
+acc_knn = knn.score(X_train_knn, Y_train_knn)
+print(f'k-Nearest Neighbors Accuracy: {round(acc_knn*100,3)}%')
+
+### Post-processing
+
+# Model Evaluation
+models = pd.DataFrame({
+    'Model': ['Decision Trees', 'Random Forest', 'Logistic Regression', 'k-Nearest Neighbors'],
+    'Score': [acc_dt, acc_rf, acc_log, acc_knn]})
+models.sort_values(by='Score', ascending=False)
