106c50ea21aaf34e22f0d66f91cb1cb587d22340
Exper1mental <Exper1mental@users.noreply.github.com>
2021-04-05 05:51:19 -0400

    Updates to Correlations
    Added code to remove any remaining rows with empty data entries.

--- Correlations.py
+++ Correlations.py
@@ -2,7 +2,7 @@
 
 # Thomas Delvaux and Anish Chaluvadi
 # ME-6930 036
-# 03/24/2021
+# 04/06/2021
 
 # Correlation code based on:
 # https://likegeeks.com/python-correlation-matrix/
@@ -51,9 +51,9 @@ print(train_df.isnull().sum())
 
 # Create Heatmap of Entries Missing Data
 # (uncomment the below lines to obtain the plot)
-# sns.heatmap(train_df.isnull())
-# plt.tight_layout()
-# plt.show()
+#sns.heatmap(train_df.isnull())
+#plt.tight_layout()
+#plt.show()
 
 
 ### Data Cleanup
@@ -79,11 +79,11 @@ for dataset in combine: # Perform this action for both the testing and training 
 
 # Plot relationship between gender, age, and pclass
 # (uncomment the below lines to obtain the plot)
-# grid = sns.FacetGrid(train_df, row='Pclass', col='Male', height=2.2, aspect=1.6)
-# grid.map(plt.hist, 'Age', alpha=.5, bins=20)
-# grid.add_legend()
-# plt.tight_layout()
-# plt.show()
+#grid = sns.FacetGrid(train_df, row='Pclass', col='Male', height=2.2, aspect=1.6)
+#grid.map(plt.hist, 'Age', alpha=.5, bins=20)
+#grid.add_legend()
+#plt.tight_layout()
+#plt.show()
 
 # Fill in missing ages
 guess_ages = np.zeros((2,3)) # Matrix to fill
@@ -111,24 +111,30 @@ for dataset in combine: # Perform this action for both the testing and training 
 
 print(train_df.head())
 
+# Checking for missing data
+#print(train_df.isnull().sum())
+#print(train_df.isnull())
 
-# Method 2 based on:
-# https://www.kaggle.com/madhumuppavarapu/titanic-train-dataset
+# Based on: https://stackoverflow.com/questions/51374068/how-to-remove-a-row-which-has-empty-column-in-a-dataframe-using-pandas
+# Removed any rows missing data. Should only remove 2 rows missing embarked data.
+train_df = train_df.dropna()
 
-# sns.boxplot(x='SibSp',y='Age',data=train_df)
-# plt.tight_layout()
-# plt.show()
+# Confirm there are no more entries missing data
+#print(train_df.isnull().sum())
 
+
+#sns.boxplot(x='SibSp',y='Age',data=train_df)
+#plt.tight_layout()
+#plt.show()
+# FILL IN OR REMOVE
 
 # Create Heatmap to Check Again for Entries Missing Data
 # (uncomment the below lines to obtain the plot)
-# sns.heatmap(train_df.isnull())
-# plt.tight_layout()
-# plt.show()
+#sns.heatmap(train_df.isnull())
+#plt.tight_layout()
+#plt.show()
 
 # Plot should show no missing entries, indicating the data is ready for use with ML
-
-
 
 
 ## Correlations
@@ -143,3 +149,11 @@ plt.xlabel("passenger features")
 plt.ylabel("passenger features")
 plt.tight_layout()
 plt.show()
+
+
+# Thomas's ML Algorithms
+# 1) Decision Trees
+
+
+# 2) Random Forest
+
